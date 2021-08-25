b9c5b404c95eecfb0632511b3e1d9991e816b81f
web-dev828 <jame.bond0523@gmail.com>
2021-08-26 03:23:38 +0800

    no message

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/url", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
+app.get("/", (req, res, next) => {
+  res.json("Hello World");
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
