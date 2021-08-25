01edc2277403a3523805b3ec8a4a769b7bd99756
web-dev828 <jame.bond0523@gmail.com>
2021-08-26 03:33:29 +0800

    no message

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+
 app.get("/url", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.get("/", (req, res, next) => {
-  res.json("Hello World");
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.listen(3000, () => {
